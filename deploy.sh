41be1fe883fca159567e6625677c9214f19198ac
Dave Lee <isho.lee@gmail.com>
2020-01-31 20:55:37 +0800

    fixed the docker build context directory

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t allguitars/multi-client:latest -t allguitars/multi-client:$SHA -f ./client/Dockerfile .
-docker build -t allguitars/multi-server:latest -t allguitars/multi-server:$SHA -f ./server/Dockerfile .
-docker build -t allguitars/multi-worker:latest -t alltuitars/multi-worker:$SHA -f ./worker/Dockerfile .
+docker build -t allguitars/multi-client:latest -t allguitars/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t allguitars/multi-server:latest -t allguitars/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t allguitars/multi-worker:latest -t alltuitars/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push allguitars/multi-client:latest
 docker push allguitars/multi-server:latest
 docker push allguitars/multi-worker:latest
